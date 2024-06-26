5921fb119a64b4d50d72d8d0b551ca1ce81ffdb5
hariharanks <hariharanks94@gmail.com>
2024-06-26 16:17:36 +0530

    alert added

--- client/src/components/UpdateCustomer.js
+++ client/src/components/UpdateCustomer.js
@@ -6,6 +6,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { updateCustomer, deleteCustomer, addCustomer } from './api';
 import { useNavigate } from "react-router-dom";
 import { useForm } from 'react-hook-form';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
 
 const UpdateCustomer = () => {
   const { id } = useParams();
@@ -50,6 +52,23 @@ const UpdateCustomer = () => {
   };
 
   const handleDelete = async () => {
+    confirmAlert({
+      title: 'Are you sure ?',
+      buttons: [
+        {
+          className:'btn-yes',
+          label: 'Yes',
+          onClick: () => deleteUser()
+        },
+        {
+          className:'btn-no',
+          label: 'No',
+        }
+      ]
+    });
+  };
+
+  const deleteUser = async ()=>{
     const result = await deleteCustomer(id);
     if (result) {
       toast.success('Customer deleted successfully');
@@ -60,7 +79,7 @@ const UpdateCustomer = () => {
     } else {
       toast.error('Failed to delete');
     }
-  };
+  }
 
   const clear = () => {
     setCustomer({ name: '', email: '', phone: '', address: '' });
